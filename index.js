3d23e86514896ee7272cc3696c70343d6c74c0f4
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-03 10:47:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+
+ function submitData(name, email){
+   let p= {
+      name,
+      email
+    }
+  
+
+let dataObject = {
+  method: "post",
+  credentials: 'same-origin',
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }, 
+  body: JSON.stringify(p)
+  
+};
+    let message = 'Unauthorized Access'
+
+fetch("http://localhost:3000/users", dataObject)
+  .then(response => response.json)
+  .then(object => {
+    for(const element of object ){
+    document.body.innerHTML = `${element.id}`
+    }
+    })
+  .catch(error => {
+    
+    document.body.innerHTML=`${message}`
+    
+  })
+}
